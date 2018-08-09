a0dae374a5f6a0d2515c125b96d15ef4bdde6388
nikosheng <sheng88509317>
2018-08-09 17:57:08 +0800

    bug fix

--- aws-contract-ecs-zip/src/main/java/com/aws/bq/zip/BqContractZipApplication.java
+++ aws-contract-ecs-zip/src/main/java/com/aws/bq/zip/BqContractZipApplication.java
@@ -1,11 +1,9 @@
 package com.aws.bq.zip;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.aws.bq.contract.dao")
 public class BqContractZipApplication {
 
     public static void main(String[] args) {
