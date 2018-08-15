35cf630b924cfcfe80a8411b4bbf535a534408d5
nikosheng <sheng88509317>
2018-08-15 11:37:48 +0800

    bug fix

--- aws-contract-ecs-zip/src/main/java/com/aws/bq/zip/config/HttpConfiguration.java
+++ aws-contract-ecs-zip/src/main/java/com/aws/bq/zip/config/HttpConfiguration.java
@@ -1,6 +1,5 @@
 package com.aws.bq.zip.config;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
